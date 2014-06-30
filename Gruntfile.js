923464a73a7f22019592ad5eb39bc4e570245451
vietduc1441 <vietduc1441@yahoo.com>
2014-06-30 14:11:11 +0200

    add jasmine to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,25 @@ module.exports = function(grunt) {
  // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+	jasmine: {
+		pivotal: {
+		  src: 'public_html/lib/*.js',
+		  options: {
+			specs: 'test/*.js',
+			display: 'full'
+		  }
+		}
+	  },
+
+	watch: {
+		tasks:['uglify'],
+		files:['public_html/js/*'],
+		options:{
+			interrupt: true,
+			reload: true
+
+		}
+	},
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
@@ -30,8 +49,10 @@ module.exports = function(grunt) {
   });
 
   // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-// Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.loadNpmTasks('grunt-contrib-watch');//watch task
+	// Default task(s).
+  grunt.registerTask('default', ['jasmine','uglify']);
 
 };
