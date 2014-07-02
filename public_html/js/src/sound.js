0cc0a0bb5217f5828a97d78880a3172ea1fb9439
vietduc1441 <vietduc1441@yahoo.com>
2014-07-02 11:19:50 +0200

    rm old files

--- public_html/js/src/sound.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', 
-function(){
-    laserSound = document.createElement("audio");
-    document.body.appendChild(laserSound);
-    laserSound.setAttribute("src", "sound/laser.mp3");
-},false);
